3a3ba74819c7a9f0fddfdaba4d7f033d292440d1
tiagorahal <rahal.aires@gmail.com>
2021-07-13 14:13:45 -0300

    Add server

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
     print: './src/print.js',
   },
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
