63728b1b35dff4d244b58d8b51b70a06b059f512
tiagorahal <rahal.aires@gmail.com>
2021-07-13 13:38:22 -0300

    Add imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
@@ -9,11 +11,14 @@ function component() {
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
-   // Add the image to our existing div.
-   const myIcon = new Image();
-   myIcon.src = Icon;
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
  
-   element.appendChild(myIcon);
+  element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
